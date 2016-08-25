f2958d3c16533a43fedc97a98fcc73a5a88bb4cd
alexwade13 <alex.wade13@gmail.com>
2016-08-25 15:37:06 -0400

    server functional, super bare

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
 var express = require('express');
+var router = require('./routes.js');
 
 var app = express();
-module.exports.app = app;
 app.set('port', 3000);
 
 // configure our server with all the middleware and routing
-require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
+// require('./config/middleware.js')(app, express);
+// require('./routes.js')(app, express);
 
 
 app.use(express.static(__dirname + '/../client'));
 
 // start listening to requests on port 8000
 app.listen(8000);
+
+console.log("listening to 8000")
+
+app.use('/*', router);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;

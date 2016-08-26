48123c51ded405c893057d2023fa7098288ca59e
Jonathan Zungre <jzungre@gmail.com>
2016-08-26 11:35:34 -0400

    continued work on the dash board

--- client/app/homeController.js
+++ client/app/homeController.js
@@ -1,8 +1,22 @@
 angular.module("makeitso", [])
-	.controller('homeController' function($scope){
+	.controller('homeController' function($scope, myFactory){
+		
+		$scope.data = {};
+
 		var getBounties = function(){
-			
+			myFactory.getAll()
+				.then(function (bounties){
+					$scope.data.bounties = bounties;
+
+
+				})
+				.catch(function(error){
+					console.error(error);
+
+				})
 
 			
 		}
+
+	getBounties();		
 	})

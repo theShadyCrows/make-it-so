84d741230a8694d2412025c07fbd22ddc8227b18
Jonathan Zungre <jzungre@gmail.com>
2016-08-29 15:06:29 -0400

    home and form views working with server

--- client/app/factory.js
+++ client/app/factory.js
@@ -1,12 +1,14 @@
 angular.module('makeitso', [])
 	.factory('myFactory', function(){
 		var getAll = function(){
+			console.lod("getAll() called!!!!!!!!!!!!!")
+
 			return $http({
 				method: 'GET',
 				url: '/bounties'
 			})
 			.then(function(resp){
-				console.log(resp.data);
+				console.log("GET response data",resp.data);
 				return resp.data;
 
 			})

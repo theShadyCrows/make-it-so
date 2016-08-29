31aada33ce741e9358103f40cc92e754a577df3a
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-08-29 17:03:21 -0400

    Added post request to pledge controller

--- client/app/pledgeController.js
+++ client/app/pledgeController.js
@@ -1,1 +1,21 @@
-pledgeController
+angular.module("makeitso.pledge", [])
+
+  .controller('pledgeController', ['$scope', '$http', function($scope, $http){
+    $scope.pledges = {};
+
+    var submitPledge = function(data){
+      console.log("submitting pledge");
+      $http({
+        method: 'POST',
+        url: '/pledge',
+        data: data
+      }).then(function (response) {
+        console.log('data',data);
+        console.log('response', response);
+        scope.data = response.data;
+        return response.data;
+        }).catch(function (error) {
+          console.log(error);
+        });
+    }
+  }]);

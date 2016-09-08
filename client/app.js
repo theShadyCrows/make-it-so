a6b972df340140bf32bba9bdfcaa474759cc8a9d
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-08 12:26:35 -0400

    Change to ui.router and seperate view files

--- client/app.js
+++ client/app.js
@@ -1,21 +1,49 @@
 angular.module('makeitso', [
 	'makeitso.form', 
 	'makeitso.home',
-	'ngRoute'
+	'ui.router'
+	// 'ngRoute'
 ])
 
-.config(function($routeProvider, $httpProvider){
-	$routeProvider
-	.when('/home', {
-		templateUrl: 'app/views/home.html',
-		// templateUrl: 'app/home.html',
-		controller: 'homeController'
-	})
-	.when('/form', {
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+  .state('home', {
+  	url: '/home',
+  	views: {
+  		'': {
+	  		templateUrl: 'app/views/home.html',
+				// templateUrl: 'app/home.html',
+				controller: 'homeController'
+  		},
+  		'widgets@home': {
+  			url: '/home',
+  			templateUrl: 'app/views/widgets.html',
+				controller: 'homeController'
+  		}
+  	}
+  })
+  .state('form', {
+  	url: '/form',
+		// templateUrl: 'app/views/form.html',
 		templateUrl: 'app/form.html',
 		controller: 'formController'
-	})
-	.otherwise({
-		redirectTo: '/home'
-	});
+  });
 });
+
+// .config(function($routeProvider, $httpProvider){
+// 	$routeProvider
+// 	.when('/home', {
+// 		templateUrl: 'app/views/home.html',
+// 		// templateUrl: 'app/home.html',
+// 		controller: 'homeController'
+// 	})
+// 	.when('/form', {
+// 		templateUrl: 'app/form.html',
+// 		controller: 'formController'
+// 	})
+// 	.otherwise({
+// 		redirectTo: '/home'
+// 	});
+// });

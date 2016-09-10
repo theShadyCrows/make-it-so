26fc4cb21edc024836163b6e0d61ed2561e150c0
H3mann <mann.harp@gmail.com>
2016-09-09 21:17:36 -0400

    merge conflict resolution

--- client/jquery.js
+++ client/jquery.js
@@ -1,22 +1,24 @@
 $( document ).ready(function() {
     console.log( "test" );
 
-  //    var obj = document.createElement("audio");
-  // console.log('OBJECT', obj);
-  //       obj.src="https://kahimyang.com/resources/sound/click.mp3";
-  //       obj.volume=0.10;
-  //       obj.autoPlay=false;
-  //       obj.preLoad=true;       
+     var obj = document.createElement("audio");
+  console.log('OBJECT', obj);
+        obj.src="http://www.wavlist.com/soundfx/010/crow-1.wav";
+        obj.volume=0.10;
+        obj.autoPlay=false;
+        obj.preLoad=true;       
  
-  //       $(".playSound").click(function() {
-  //       	console.log('clicked')
-  //           obj.play();
-  //       });
+        $("bountyBtn1").on("click",function() {
+        	console.log('ALL BUTTONS')
+            obj.play();
+        });
+
+        // $("makeitso-button").on("click")
 
  
-$( ".playSound" ).on( "click", function() {
-  console.log('CLICKED');
-});
+// $( "#makeitso-button" ).on( "click", function() {
+//   console.log('CLICKED');
+// });
 
 
 
